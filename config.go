1151b305eb9669a80df58890fec6275af5aec8e0
Trygve Aaberge <trygveaa@gmail.com>
2014-05-06 16:03:16 +0200

    Add some comments

--- config.go
+++ config.go
@@ -25,6 +25,7 @@ func loadConfig(filename string) (config Config, err error) {
 }
 
 func loadConfigWithDefaults(filename string, defaultConfig *Config) error {
+	// Use reflect to place config keys into the right element in the struct
 	configReflect := reflect.ValueOf(defaultConfig).Elem()
 
 	f, err := os.Open(filename)
@@ -37,32 +38,44 @@ func loadConfigWithDefaults(filename string, defaultConfig *Config) error {
 		var key, value string
 		line := fh.Text()
 		lineParts := strings.Split(line, "\"")
+		// Split the line on ", because we want to keep parts
+		// inside "" unchanged.
 		for i, part := range lineParts {
 			if i%2 == 0 {
 				commentIndex := strings.Index(part, "#")
 				if commentIndex != -1 {
+					// Remove comments
 					part = part[:commentIndex]
 				}
 				if i == 0 {
+					// If first part, we want to fetch the key
 					keyVal := strings.SplitN(part, "=", 2)
 					key = strings.TrimSpace(keyVal[0])
 					if len(keyVal) < 2 {
+						// The part didn't contain a =
 						if i != len(lineParts)-1 && commentIndex == -1 {
+							// Not the last line, which means there is a " before the = (if any)
 							return fmt.Errorf("\" are not allowed in key: %s", line)
 						}
 						if key != "" {
+							// Last line, which means no =
 							return fmt.Errorf("Config line must contain \"=\": %s", line)
 						}
+						// The line is only comments
 						break
 					} else if key == "" {
+						// Line had a =, but only spaces before it
 						return fmt.Errorf("Key can't be empty: %s", line)
 					}
+					// We want to trim space at the start of the value
 					part = strings.TrimLeftFunc(keyVal[1], unicode.IsSpace)
 				}
 				if i == len(lineParts)-1 || commentIndex != -1 {
+					// Last part, we want to trim space at the end of the value
 					part = strings.TrimRightFunc(part, unicode.IsSpace)
 				}
 				if commentIndex != -1 {
+					// The part had a comment char, ignore the rest of the parts
 					value += part
 					break
 				}
@@ -71,6 +84,7 @@ func loadConfigWithDefaults(filename string, defaultConfig *Config) error {
 		}
 
 		if key == "" {
+			// The line is only comments
 			continue
 		}
 
@@ -78,6 +92,7 @@ func loadConfigWithDefaults(filename string, defaultConfig *Config) error {
 			return fmt.Errorf("Value of key \"%s\" can't be empty.", key)
 		}
 
+		// Fetch the field in the config struct with the same name as the key
 		field := configReflect.FieldByName(key)
 		if !field.IsValid() {
 			return fmt.Errorf("Config key is not valid: %s", key)
